c2a7a9a728ac8d72163ae656f3d412e20d5318b5
Thais Moura <tfreitas@thoughtworks.com>
2018-12-22 13:05:29 -0300

    add jest testing example project

---
+++ __tests__/subtract.js
@@ -0,0 +1,5 @@
+import subtract from '../subtract';
+
+test('subtract two numbers', () => {
+  expect(subtract(5, 3)).toBe(2)
+})
---
+++ __tests__/sum.js
@@ -0,0 +1,5 @@
+import sum from '../sum';
+
+test('add two numbers', () => {
+  expect(sum(1,3)).toBe(4)
+})
